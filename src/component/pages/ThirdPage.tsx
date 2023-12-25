89e839b841332e656b02d8d6de13f8ec8138e0a3
SEIMIN-BJJ <interkq12@gmail.com>
2023-12-26 07:44:40 +0900

    .

--- src/component/pages/ThirdPage.tsx
+++ src/component/pages/ThirdPage.tsx
@@ -1,18 +1,17 @@
 import React, { useState, useEffect, forwardRef } from 'react';
 import styled from 'styled-components';
 import Slider from 'react-slick';
+import { motion } from 'framer-motion';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import RedHotIMG from '../assets/images/redhot.png';
+import AskingIMG from '../assets/images/asking.png';
+import CrossFaithIMG from '../assets/images/cross.png';
+import FearAndIMG from '../assets/images/fear.png';
+import MaximumIMG from '../assets/images/maximum.png';
+import RingoIMG from '../assets/images/ringgo.png';
+import SoilPimpIMG from '../assets/images/soil.png';
 import '../../slick-theme.css';
 import '../../slick.css';
-import { motion } from 'framer-motion';
-import RedHotIMG from '../assets/images/red-hot-chili-peppers.jpeg';
-import AskingIMG from "../assets/images/asking-alexandria.jpeg";
-import CrossFaithIMG from "../assets/images/cross-faith.jpeg";
-import FearAndIMG from "../assets/images/fear-and-loathing.webp";
-import MaximumIMG from "../assets/images/maximum-the-hormone.webp";
-import SheenaRingoIMG from "../assets/images/sheena-ringo.jpeg";
-import SoilPimpIMG from "../assets/images/soil-pimp-sessons.jpeg";
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-
 
 const ThirdContent = styled.section`
   width: 100vw;
@@ -51,16 +50,50 @@ const SliderContainer = styled(motion.div)`
   width: 80%;
 `;
 
+const ImageWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
+  transition: transform 0.3s ease-out;
+
+  &:hover {
+    transform: scale(1.1);
+    z-index: 1;
+  }
+`;
+
+const ImageText = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  color: #fff;
+  padding: 1rem;
+  letter-spacing: 0.05rem;
+  text-align: center;
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+  z-index: 2;
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+
+  ${ImageWrapper}:hover & {
+    opacity: 1;
+    z-index: 2;
+  }
+`;
+
 const SliderItem = styled(motion.div)<{ Images: string }>`
   height: 20rem;
   background-size: contain;
   background-position: center center;
   background-repeat: no-repeat;
   background-image: url(${(props) => props.Images});
+  position: relative;
+  margin: 0 0.3rem;
 `;
 
 const CustomArrow = styled.div`
-  width: 5rem;
+  width: 4rem;
   height: 3rem;
   font-size: 2rem;
   color: #fff;
@@ -78,26 +111,21 @@ const CustomArrow = styled.div`
   &:hover {
     opacity: 1;
     color: #ccc;
-    background-color: #000000;
   }
 `;
 
+const images = [
+  { img: RedHotIMG, text: 'RED HOT CHILI PEPPERS' },
+  { img: AskingIMG, text: 'ASKING ALEXANDRIA' },
+  { img: CrossFaithIMG, text: 'CROSS FAITH' },
+  { img: FearAndIMG, text: 'FEAR AND LOATHING IN LASVEGAS' },
+  { img: MaximumIMG, text: 'MAXIMUM THE HORMONE' },
+  { img: RingoIMG, text: 'SHIINA RINGO' },
+  { img: SoilPimpIMG, text: 'SOIL & PIMP SESSIONS' },
 
-const images = [ 
-  RedHotIMG,
-  AskingIMG,
-  CrossFaithIMG,
-  FearAndIMG,
-  MaximumIMG,
-  SheenaRingoIMG,
-  SoilPimpIMG,
 ];
 
 const ThirdPage = forwardRef<HTMLDivElement>((props, ref) => {
-
-  interface ArrowProps {
-    onClick?: () => void;
-  }
   const [animate, setAnimate] = useState(false);
 
   const animationLeft = {
@@ -120,13 +148,13 @@ const ThirdPage = forwardRef<HTMLDivElement>((props, ref) => {
     delay: 0.3,
   };
 
-  const PrevArrow = ({ onClick }: ArrowProps) => (
+  const PrevArrow = ({ onClick }: { onClick?: () => void }) => (
     <CustomArrow className="slick-prev" onClick={onClick}>
       <FaChevronLeft />
     </CustomArrow>
   );
-  
-  const NextArrow = ({ onClick }: ArrowProps) => (
+
+  const NextArrow = ({ onClick }: { onClick?: () => void }) => (
     <CustomArrow className="slick-next" onClick={onClick}>
       <FaChevronRight />
     </CustomArrow>
@@ -139,8 +167,8 @@ const ThirdPage = forwardRef<HTMLDivElement>((props, ref) => {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: true, 
+    autoplaySpeed: 3000,
+    arrows: true,
     prevArrow: <PrevArrow />,
     nextArrow: <NextArrow />,
   };
@@ -177,7 +205,7 @@ const ThirdPage = forwardRef<HTMLDivElement>((props, ref) => {
 
   return (
     <ThirdContent ref={ref}>
-    <ThirdSection>
+      <ThirdSection>
         <ThirdTitle
           variants={animationLeft}
           initial="hidden"
@@ -186,24 +214,25 @@ const ThirdPage = forwardRef<HTMLDivElement>((props, ref) => {
         >
           ARTIST
         </ThirdTitle>
-    <SliderContainer 
-      ref={ref}
-      variants={animationRight}
-      initial="hidden"
-      animate={animate ? 'visible' : 'hidden'}
-      transition={transitionSlide}>
-
-      <Slider {...settings}>
-        {images.map((image, index) => (
-          <SliderItem
-            key={index}
-            Images={image}
-          />
-        ))}
-      </Slider>
-    </SliderContainer>
-    </ThirdSection>
-  </ThirdContent>
+        <SliderContainer
+          ref={ref}
+          variants={animationRight}
+          initial="hidden"
+          animate={animate ? 'visible' : 'hidden'}
+          transition={transitionSlide}
+        >
+          <Slider {...settings}>
+          {images.map((item, index) => (
+            <ImageWrapper key={index}>
+              <SliderItem Images={item.img}>
+                <ImageText>{item.text}</ImageText>
+              </SliderItem>
+            </ImageWrapper>
+          ))}
+          </Slider>
+        </SliderContainer>
+      </ThirdSection>
+    </ThirdContent>
   );
 });
 
